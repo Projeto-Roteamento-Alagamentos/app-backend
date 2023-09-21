5e9fbaa8cc09c912e53c8b0ac4725fb9db08c59a
Victor Pedrota <v.pedrota@unifesp.br>
2023-09-20 23:34:35 -0300

    Feat: rotas para são paulo

--- app/api/v1/modelo_previsao/views.py
+++ app/api/v1/modelo_previsao/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, jsonify, request 
+from flask import Blueprint, request, jsonify
+import osmnx as ox
+import networkx as nx
+import geopandas as gpd
 import json
 
 modelo_blueprint = Blueprint('modelo_previsao', __name__)
+
+graph_filename = "./app/api/v1/modelo_previsao/graph.graphml"
 
 @modelo_blueprint.route('/geojson', methods=['POST'])
 def handle_geojson():
@@ -13,22 +18,28 @@ def handle_geojson():
 
     data = json.loads(data)
 
-    if 'type' not in data or data['type'] != 'FeatureCollection':
-        return jsonify({"error": "Invalid GeoJSON"}), 400
-
+    graph = ox.load_graphml(graph_filename)
+    
+    
+    data = request.get_json()
     point1 = data['features'][0]['geometry']['coordinates']
     point2 = data['features'][1]['geometry']['coordinates']
+    
+    
+    node_start = ox.distance.nearest_nodes(graph, X=point1[1], Y=point1[0])
+    node_end = ox.distance.nearest_nodes(graph, X=point2[1], Y=point2[0])
+    
+    route = nx.astar_path(graph, node_start, node_end, weight='length')
 
-    linestring =  {
-    "type": "FeatureCollection",
-    "features": [{
+    coordenadas = [(graph.nodes[n]['x'], graph.nodes[n]['y']) for n in route]
+
+    geojson = {
         "type": "Feature",
         "geometry": {
             "type": "LineString",
-            "coordinates": [[point1[1], point1[0]], [point2[1], point2[0]]]
-        }
+            "coordinates": coordenadas
+        },
+        "properties": {}
     }
-    ]
-  } 
 
-    return jsonify(linestring), 200
+    return jsonify(geojson), 200
